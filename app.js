af62f2707c06348f44fdeb808e893c404f7bbb19
Diaaana03 <88292313+Diaaana03@users.noreply.github.com>
2023-08-22 23:17:23 +0100

    product js added

--- app.js
+++ app.js
@@ -20,18 +20,20 @@ const displayProducts = (list) => {
       const { name: title, price } = product.fields;
       const { url: img } = product.fields.image[0];
       const formatPrice = price / 100;
-      return `<a class="single-product" href="product.html" >
-            <img src="${img}" alt="${title}" class="single-product-img img" />
+      // id,name,price,img
+      return `<a class="single-product" href="product.html?id=${id}&name=john&age=25">
+            <img src="${img}" class="single-product-img img" alt="${title}" />
             <footer>
               <h5 class="name">${title}</h5>
-              <span class="price">${formatPrice}</span>
+              <span class="price">$${formatPrice}</span>
             </footer>
           </a>`;
     })
     .join("");
-  productsDOM.innerHTML = `<div class="product-container">
-  ${productList}
-  </div>`;
+  productsDOM.innerHTML = ` <div class="products-container">
+         ${productList}
+          
+        </div>`;
 };
 
 const start = async () => {
