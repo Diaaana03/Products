516de74fe52e6b1cc91adb8dcca2d2dcfb13458c
Diaaana03 <88292313+Diaaana03@users.noreply.github.com>
2023-08-22 01:09:47 +0100

    app js code added

--- app.js
+++ app.js
@@ -14,7 +14,24 @@ const fetchProducts = async () => {
 };
 
 const displayProducts = (list) => {
-  console.log(list);
+  const productList = list
+    .map((product) => {
+      const { id } = product;
+      const { name: title, price } = product.fields;
+      const { url: img } = product.fields.image[0];
+      const formatPrice = price / 100;
+      return `<a class="single-product" href="product.html" >
+            <img src="${img}" alt="${title}" class="single-product-img img" />
+            <footer>
+              <h5 class="name">${title}</h5>
+              <span class="price">${formatPrice}</span>
+            </footer>
+          </a>`;
+    })
+    .join("");
+  productsDOM.innerHTML = `<div class="product-container">
+  ${productList}
+  </div>`;
 };
 
 const start = async () => {
